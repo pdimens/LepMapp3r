7cfd00ea1af2b1f947221cc5a8e2551ff54f3503
Pavel V. Dimens <pdimens@live.com>
2019-05-28 00:44:52 -0500

    Update and rename install_LepMapp3r.sh to LM3.install.sh

--- LM3.install.sh
+++ LM3.install.sh
@@ -1,22 +1,21 @@
 #! /usr/bin/env bash
 
 echo "Downloading LepMap3 binaries from the official SourceForge repository..."
-wget https://downloads.sourceforge.net/project/lep-map3/binary%2Bcode.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flep-map3%2Ffiles%2F&ts=1559020265&use_mirror=pilotfiber
+wget -O LepMap3.zip "https://downloads.sourceforge.net/project/lep-map3/binary%2Bcode.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flep-map3%2Ffiles%2F&ts=1559020265&use_mirror=pilotfiber"
 
 echo "Unzipping LepMap3..."
-mkdir LepMap3
-unzip binary* -d ./LepMap3 
+unzip LepMap3.zip 
 
 echo "Copying files to PATH locations..."
 mkdir LM3
 chmod +x LepMapp3r/*
-cp LepMap3/bin/* LepMapper/LepMapp3rQA.r LM3
+cp ./bin/* LepMapper/LepMapp3rQA.r LM3
 sudo mv LM3 LepMapp3r /bin
 
 echo "[Installation summary]"
 echo -e "File\tLocation"
 echo -e "LepMapp3r\t/bin"
-for LM3FILE in /bin/LM3/*
+for LM3FILE in $(ls /bin/LM3/*)
 do
     echo -e "$LM3FILE\t$(dirname $LM3FILE)"
 done
