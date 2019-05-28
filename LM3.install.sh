f73addf801f0a789c72bb9706b063bb09d10d5e1
Pavel V. Dimens <pdimens@live.com>
2019-05-28 01:02:39 -0500

    Update LM3.install.sh

--- LM3.install.sh
+++ LM3.install.sh
@@ -4,7 +4,7 @@ echo "Downloading LepMap3 binaries from the official SourceForge repository..."
 wget -O LepMap3.zip "https://downloads.sourceforge.net/project/lep-map3/binary%2Bcode.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flep-map3%2Ffiles%2F&ts=1559020265&use_mirror=pilotfiber"
 
 echo "Unzipping LepMap3..."
-unzip LepMap3.zip 
+unzip -qq LepMap3.zip 
 
 echo "Copying files to PATH locations..."
 mkdir LM3
