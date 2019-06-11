c914335a120eaf16c0253d2f5487af2e012213ff
Pavel V. Dimens <pdimens@live.com>
2019-06-11 13:47:38 -0500

    add lepmap2allmaps

--- LM3.install.sh
+++ LM3.install.sh
@@ -10,11 +10,12 @@ echo "Copying files to PATH locations..."
 mkdir LM3
 chmod +x ./*
 cp ./bin/* LepMapp3rQA.r LM3
-sudo mv LM3 LepMapp3r /bin
+sudo mv LM3 LepMapp3r lepmap2allmaps /bin
 
 echo "[Installation summary]"
 echo -e "Location\tFile"
 echo -e "/bin\tLepMapp3r"
+echo -e "/bin\tlepmap2allmaps"
 for LM3FILE in $(ls /bin/LM3/*)
 do
     echo -e "$(dirname $LM3FILE)\t$(basename $LM3FILE)"
