7abaade7e1f9af4ffed928afb1be7242affab7fe
Pavel V. Dimens <pdimens@live.com>
2020-04-24 09:29:28 -0500

    Update LepMapp3rQA.r

--- LepMapp3rQA.r
+++ LepMapp3rQA.r
@@ -1,3 +1,5 @@
+#! /usr/bin/env Rscript
+
 args = commandArgs(trailingOnly = TRUE)
 
 suppressMessages(if (!require("dplyr")) install.packages("dplyr"))
@@ -13,11 +15,11 @@ par(mfrow=(c(4,2))) # create 4x2 plots
 ##### Pruning the ends #####
 for(i in file.names){  
   lgfile <- read.delim(
-              paste(path,i, sep = "/"), 
-              header = FALSE, 
-              sep = "\t", 
-              comment.char="#"
-            )
+    paste(path,i, sep = "/"), 
+    header = FALSE, 
+    sep = "\t", 
+    comment.char="#"
+  )
   
   # instantiate QC columns
   lgfile$Mpass <- c(TRUE)
@@ -27,7 +29,7 @@ for(i in file.names){
     # trim beginning
     filelength15 <- length(lgfile$V1) * 0.15
     for(a in 1:filelength15){ #first 15% of total markers from the beginning
-      diff <- abs(passing_markers[a+1,j]-passing_markers[a,j]) # difference between two points
+      diff <- abs(lgfile[a+1,j]-lgfile[a,j]) # difference between two points
       if( diff > 10 ){ # is the difference between the two points > distance argument?
         lgfile[1:a, j+4] <- FALSE
       }
@@ -35,15 +37,15 @@ for(i in file.names){
     # trim end
     filelen<-length(lgfile$V1)  # get new file lengths for each time we remove NA's
     for(z in filelen:(filelen-filelength15)){  #iterate 15% total markers in starting from the end
-      diff <- abs(passing_markers[z,j]-passing_markers[z-1,j]) # difference between two points
+      diff <- abs(lgfile[z,j]-lgfile[z-1,j]) # difference between two points
       if( diff > 10 ){ # is the difference between the two points > distance argument?
         lgfile[filelen:z,j+4] <- FALSE # mark that marker and all markers AFTER it as NA
       }
     }
-
+    
     # create new table of markers passing QC
     cleaned_markers <- lgfile %>% filter(Mpass == TRUE & Fpass == TRUE)  
-
+    
     # diagnostic plots
     par(mar=c(3,4.3,2,1)+0.1)  # reduce the padding somewhat
     just_ordernum <- tools::file_path_sans_ext(i)  # remove the extension from files for plots
@@ -69,11 +71,11 @@ for(i in file.names){
       )
       points(x = which(lgfile$Fpass == FALSE), y = lgfile$V3[lgfile$Fpass == FALSE] , col = "coral3", pch = 19, cex = .8 )
     }
-   }
+  }
   
   # isolate bad markers
   removed_markers <- (lgfile %>% filter(Mpass == FALSE | Fpass == FALSE))$V1 
-
+  
   # outputting filtered files
   filename<- paste("trimmed",i, sep=".")
   print(paste("Removing",length(removed_markers),"markers from",i , "and writing new file", filename, sep = " "))
